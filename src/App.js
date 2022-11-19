cb9b60e5b5c8772616e87dd3d941de10c0844484
Wayne Haworth <waynehaworth@gmail.com>
2022-11-19 20:04:30 +0000

    add search

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const SearchResults = ({countries, searchText}) => {
+  
+  const filteredCountries = countries.filter(country => country.name.common.toLowerCase().includes(searchText.toLowerCase()))
+  
+  if (searchText === '') { return <p>Search for a country</p> }
+  else if (filteredCountries.length > 10) { return <p>Too many matches, specify another filter</p> }
+  else if (filteredCountries.length === 1) { return <RenderCountry country={filteredCountries[0]}/> }
+  else { return filteredCountries.map(country => { return <RenderCountries country={country} countries={countries} searchText={searchText} /> }) }
+}
+
+const RenderCountries = ({country}) => {
+  const [detailsToggle,setDetailsToggle] = useState(false);
+  
+  return (
+    <div>
+      <div key={country.name.cca3}>
+        {country.name.common}  
+        {/* note if toggle button is true, show below*/}
+
+      </div>
+    </div>
+  )
+}
+
+const RenderCountry = ({country}) => {
+  console.log(country)
+  console.log(country.flags.png)
+  return (
+    <div key={country.name.common}>
+      <h1>{country.name.common}</h1>
+      <p>{country.capital}</p>
+      <p>{country.area}</p>
+
+      <h2>languages:</h2>
+      <ul>
+        {Object.entries(country.languages).map(([key, value]) => { return <li key={key}>{value}</li> } )}
+      </ul>
+      <img src={country.flags.png} />
+    </div>
+  )
+}
 
 function App() {
+  const [countries, setCountries] = useState([])
+  const [searchText, setSearchText] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('https://restcountries.com/v3.1/all')
+      .then(response => {
+        const countries = response.data  
+        setCountries(countries)
+      })
+  }, [])
+
+  const handleSearchText = (event) => {
+    setSearchText(event.target.value)
+  }  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input placeholder="Enter a country" value={searchText} onChange={handleSearchText}/>     
+      <SearchResults countries={countries} searchText={searchText}/>               
     </div>
   );
 }
